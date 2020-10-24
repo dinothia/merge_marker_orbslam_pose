37a5fef16c6b03d36ccc3a939f74bb86cfdbcfc7
dinothia <dinossht@stud.ntnu.no>
2020-10-24 16:58:08 +0200

    update bash script

--- run_marker_orbslam.sh
+++ run_marker_orbslam.sh
@@ -2,10 +2,10 @@
 # SLAM
 gnome-terminal --tab -- bash -c "roscore"
 gnome-terminal --tab -- bash -c "cd ~/Installs/ORB_SLAM3 && rosrun ORB_SLAM3 Mono Vocabulary/ORBvoc.txt Examples/Monocular/Kaia.yaml"
-gnome-terminal --tab -- bash -c "sleep 2 && rosrun image_transport republish compressed in:=/camera/image_raw raw out:=/camera/image_raw"
+gnome-terminal --tab -- bash -c "echo \"image_transport compressed to raw\" && sleep 2 && rosrun image_transport republish compressed in:=/camera/image_raw raw out:=/camera/image_raw"
 
 # Marker
-gnome-terminal --tab -- bash -c "sleep 1 && python3 ~/Documents/Prosjektoppgave/Code/charuco_pose_estimation/main.py /camera/image_raw/compressed ~/Documents/Prosjektoppgave/Code/charuco_pose_estimation/cameraParams.yaml ~/Documents/Prosjektoppgave/Code/charuco_pose_estimation/cameraTrajectory.txt" 
+gnome-terminal --tab -- bash -c "echo \"Marker based pose estimation\" && sleep 1 && python3 ~/Documents/Prosjektoppgave/Code/charuco_pose_estimation/main.py /camera/image_raw/compressed ~/Documents/Prosjektoppgave/Code/charuco_pose_estimation/cameraParams.yaml ~/Documents/Prosjektoppgave/Code/charuco_pose_estimation/cameraTrajectory.txt" 
 
 # Run bag file
-gnome-terminal --tab -- bash -c "sleep 10 && rosbag play -r 0.5 ~/Datasets/Kaia/trondheim2_inn.bag -s 800"
+gnome-terminal --tab -- bash -c "sleep 10 && rosbag play -r $3 ~/Datasets/Kaia/$1 -s $2"
